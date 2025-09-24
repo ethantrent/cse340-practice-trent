8f593afe7a3a77f0799f3799e7f06c4bd80f2a47
Ethan <ethanjotrent@gmail.com>
2025-09-23 21:56:02 -0600

    Added NODE_ENV variable

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import path from 'path';
 /**
  * Declare Important Variables
  */
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -27,22 +28,24 @@ app.set('view engine', 'ejs');
 
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
+
 /**
- * Declare Routes
+ * Routes
  */
 app.get('/', (req, res) => {
     const title = 'Welcome Home';
     res.render('home', { title });
 });
+
 app.get('/about', (req, res) => {
     const title = 'About Me';
     res.render('about', { title });
 });
+
 app.get('/products', (req, res) => {
     const title = 'Our Products';
     res.render('products', { title });
 });
-
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
